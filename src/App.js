8d322369ca65571df0075993f19729447da8df80
agsleonardo <agsleonardo@hotmail.com>
2022-02-22 18:46:47 -0300

    test audio

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
     
   pause = () => {
   this.setState({ play: false, pause: true })
-    this.audio.stop();
+    this.audio.pause();
   }    
 
   interval = null;
