07af8e0b2820ed0c93ba646288c8900d587558d6
agsleonardo <agsleonardo@hotmail.com>
2022-02-22 20:43:11 -0300

    fix: random musics while counting

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ export default class App extends Component {
     pause: true,
   }
   radios = ['https://listen.christianhardrock.net/stream/3',
-'https://uk7.internet-radio.com/proxy/movedahouse?mp=/stream;',
-'https://r163-172-186-93.relay.radiotoolkit.com:30003/rcmdeep']
+'https://uk7.internet-radio.com/proxy/movedahouse?mp=/stream;']
   url = this.radios[Math.floor(Math.random() * (2 - 0)) + 0];
   audio = new Audio(this.url);
   play = () => {
