7fb6ac830fe63b55904fd6b76795966a5886c34a
agsleonardo <agsleonardo@hotmail.com>
2022-02-22 22:23:03 -0300

    fix: stream url

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ export default class App extends Component {
     play: false,
     pause: true,
   }
-  radios = ['https://listen.christianhardrock.net/stream/3',
-'https://uk7.internet-radio.com/proxy/movedahouse?mp=/stream']
-  url = this.radios[Math.floor(Math.random() * (1 - 0)) + 0];
+//   radios = ['https://listen.christianhardrock.net/stream/3',
+// 'https://uk7.internet-radio.com/proxy/movedahouse?mp=/stream']
+  // url = this.radios[Math.floor(Math.random() * (1 - 0)) + 0];
+  url = 'https://uk7.internet-radio.com/proxy/movedahouse?mp=/stream';
   audio = new Audio(this.url);
   play = () => {
     this.setState({ play: true, pause: false })
